9720b054f09209813f7ea5b9619db7957affa66a
jeffersonreis <33864468+jeffersonreis@users.noreply.github.com>
2022-06-20 21:24:33 -0300

    Update localizationRoutes.js

--- routes/localizationRoutes.js
+++ routes/localizationRoutes.js
@@ -39,25 +39,26 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// router.patch('/:id', async(req,res) => {
-//     const id = req.params.id
+router.patch("/:id", async (req, res) => {
+  const id = req.params.id;
 
-//     const {placa, ano, modelo, aluguel} = req.body
-//     const car = {placa, ano, modelo, aluguel}
+  const { latitude, longitude, carro } = req.body;
+  const loc = { latitude, longitude, carro };
 
-//     try {
-//         const updatedCar = await Car.updateOne({_id:id}, car)
-//         if (updatedCar.matchedCount === 0) {
-//             res.status(422).json({message:"O carro não foi encontrado!"})
-//             return
-//         }
+  try {
+    const updatedLoc = await loc.updateOne({ _id: id }, loc);
+    if (updatedLoc.matchedCount === 0) {
+      res
+        .status(422)
+        .json({ message: "Nenhuma localização encontrada para esse ID!" });
+      return;
+    }
 
-//         res.status(200).json(car)
-
-//     } catch(error) {
-//         res.status(500).json({error: error})
-//     }
-// })
+    res.status(200).json(loc);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
 
 // router.delete('/:id', async(req,res) => {
 //     const id = req.params.id
